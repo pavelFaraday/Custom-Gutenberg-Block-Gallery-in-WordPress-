ea5e049a83854fe1994348f5611713c719f29e30
“pavelFaraday” <devstudio.corp@gmail.com>
2025-05-15 14:12:17 +0400

    Custom Gutenberg Block - Style Gallery images

--- app/public/wp-content/plugins/fade-zoom-slider/build/fade-zoom-slider/index.asset.php
+++ app/public/wp-content/plugins/fade-zoom-slider/build/fade-zoom-slider/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '940c27e26b1b8d2c719e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '4a1d22205c5ebcb95317');
