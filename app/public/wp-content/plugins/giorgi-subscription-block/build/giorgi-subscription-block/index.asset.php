4bba70b6c469d726118a9d04a24b14b2375d9082
“pavelFaraday” <devstudio.corp@gmail.com>
2025-05-27 20:53:40 +0400

    Custom Gutenberg Block | Project 3
    - Create Subscription Block | Install initial files

---
+++ app/public/wp-content/plugins/giorgi-subscription-block/build/giorgi-subscription-block/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '7e2007b04f20fd4ba8bb');
---
+++ app/public/wp-content/plugins/giorgi-subscription-block/build/giorgi-subscription-block/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '9df5822e4e09a6234b87');
