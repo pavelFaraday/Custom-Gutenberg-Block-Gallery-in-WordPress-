13586bfb47cc26340ffc621a61f4d5c0e381cca4
“pavelFaraday” <devstudio.corp@gmail.com>
2025-06-15 18:12:45 +0400

    Custom Gutenberg Block | Project 4 | Gutenberg First Block Starter
    - Install Initial files

---
+++ app/public/wp-content/plugins/first-block/build/blocks-manifest.php
@@ -0,0 +1,25 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'first-block' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'create-block/first-block',
+		'version' => '0.1.0',
+		'title' => 'First Block',
+		'category' => 'widgets',
+		'icon' => 'smiley',
+		'description' => 'Example block scaffolded with Create Block tool.',
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'textdomain' => 'first-block',
+		'editorScript' => 'file:./index.js',
+		'editorStyle' => 'file:./index.css',
+		'style' => 'file:./style-index.css',
+		'viewScript' => 'file:./view.js'
+	)
+);
