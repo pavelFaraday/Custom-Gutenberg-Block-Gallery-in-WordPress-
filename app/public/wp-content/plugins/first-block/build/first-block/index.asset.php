7359e56f4f7b3ca1cd9b372d95efe8eda182b16d
“pavelFaraday” <devstudio.corp@gmail.com>
2025-06-15 19:10:46 +0400

    Custom Gutenberg Block | Project 5 | Gutenberg First Block Starter
    - Refactor block registration code and update asset version

--- app/public/wp-content/plugins/first-block/build/first-block/index.asset.php
+++ app/public/wp-content/plugins/first-block/build/first-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'a250118a3fdb6d1051ef');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'e94c1bd54c517acf1bb6');
